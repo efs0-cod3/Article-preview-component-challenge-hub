5ff11461a3e02135662cdc6f442c936dfd41f309
efs0-cod3 <eflearnng@gmail.com>
2022-01-01 06:42:37 -0400

    button added an icon cdn to simple things!

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let button = document.querySelector(".share-btn")
 let show = document.getElementById("soc")
+let share_btn = document.getElementById('s-btn')
 
 
 button.addEventListener("focusin", function(){
 show.style.visibility = "visible";
+share_btn.style.color = "hsl(210, 46%, 95%)";
+share_btn.style.background = "hsl(212, 23%, 69%)";
 })
 
 button.addEventListener("focusout", function(){
 show.style.visibility = "hidden";
-
+share_btn.style.color = "";
+share_btn.style.background = "";
 })
+
+    
